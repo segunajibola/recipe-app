8a05fe11119b53202d7fce31ef7dc4f3377c5d44
Segun Ajibola <ajbl.segun@gmail.com>
2021-10-13 13:09:44 +0100

    fetch data

---
+++ script.js
@@ -0,0 +1,48 @@
+const meals = document.getElementById("meals");
+
+getRandomMeal();
+
+async function getRandomMeal() {
+    const resp = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+    
+    const respData = await resp.json();
+
+    const randomMeal = respData.meals[0];
+
+    addMeal(randomMeal, true)
+}
+
+async function getMealbyId(id) {
+    const meal = await fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id);
+}
+
+async function getMealsBySearch(term) {
+    const meals = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + term);
+}
+
+function addMeal(mealData, random = false) {
+    const meal = document.createElement("div");
+    meal.classList.add("meal");
+
+    meal.innerHTML = `
+                <div class="meal-header">
+                    ${random ? `<span class="random">Random Recipe</span>` : ""}
+                    <img src="${mealData.strMealThumb}" alt="${mealData.Meal}">
+                </div>
+                <div class="meal-body">
+                    <h4>${mealData.Meal}</h4>
+                    <button class="fav-btn">
+                        <i class="fas fa-heart"></i>
+                    </button>
+                </div>
+                `;
+                
+                const btn = meal.querySelector(".meal-body .fav-btn");
+                btn.addEventListener("click", () => {
+                    btn.classList.toggle
+                    ("active");
+                });
+                
+
+                meals.appendChild(meal);
+}

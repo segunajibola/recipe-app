414cc31a6369efab4389ba041d5ef99337a817cf
Segun Ajibola <ajbl.segun@gmail.com>
2021-11-10 02:13:25 +0100

    search

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const mealsEl = document.getElementById("meals");
 const favouriteContainer = document.getElementById("fav-meals");
 const mealPopup = document.getElementById("meal-popup");
+const mealInfoEl = document.getElementById("meal-info");
 const popupCloseBtn = document.getElementById("close-popup");
 const searchTerm = document.getElementById("search-term");
 const searchBtn = document.getElementById("search");
@@ -147,6 +148,16 @@ function addMealFav(mealData) {
     favouriteContainer.appendChild(favMeal);
 }
 
+function showMealInfo(mealData) {
+    //update meal info
+    const mealEl = document.createElement("div");
+
+    mealInfoEl.appendChild(mealEl);
+
+    mealEl.innerHTML = `
+    `
+}
+
 searchBtn.addEventListener("click", async () => {
     // clean container
     mealsEl.innerHTML = "";
@@ -160,3 +171,7 @@ searchBtn.addEventListener("click", async () => {
         });
     }
 });
+
+popupCloseBtn.addEventListener("click", () => {
+    mealPopup.classList.add("hidden");
+})
